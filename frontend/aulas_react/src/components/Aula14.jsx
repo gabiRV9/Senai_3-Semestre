4412799abd9232ce781ab2533561c6189db4aa31
Gabriella <gabriella.vicente@aluno.senai.br>
2026-03-26 15:20:54 -0300

    Aula26/03/2026

---
+++ frontend/aulas_react/src/components/Aula14.jsx
@@ -0,0 +1,29 @@
+import { estilos } from "../style/Estilos"
+import { Link, useNavigate } from 'react-router-dom'
+
+const Aula14 = () => {
+    const navigate = useNavigate();
+    return (
+        <div style={estilos.cardAula}>
+            <h2>Aula 14 - React Router - Navegação em React</h2>
+            <h3>Biblioteca que permite criar e gerenciar rotas em React</h3>
+            <hr />
+            <h3>Navegação com Links do React Router</h3>
+            <Link to='/'> Página Principal </Link>
+            <br />
+            <Link to='/sobre'> Sobre Gabriella </Link>
+            <br />
+            <Link to='/sesisenai'> Página Inexistente </Link>
+            <br />
+            <h3>Navegação com progamação utilizando o useNavigate</h3>
+            <button onClick={() => navigate('/sobre') } >Sobre</button>
+
+            <hr />
+            <h3>Rota dinâmica com useParams</h3>
+            <button onClick={() => navigate('/perfil/Gabriella') } >Perfil da Gabriella</button>
+            <button onClick={() => navigate('/perfil/Clara') } >Perfil da Clara</button>
+        </div>
+    )
+}
+
+export default Aula14
